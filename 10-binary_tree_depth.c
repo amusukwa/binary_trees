2d2d00df30011dd3cc16e9afbf8febe0d4a70763
amusukwa <amusukwa@gmail.com>
2023-11-01 05:41:18 -0700

    11-binary_tree_size.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,18 +10,17 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (tree == NULL)
-    {
-        return (0);  /* If node is NULL, its depth is 0 */
-    }
+	if (tree == NULL)
+	{
+	return (0);
+	}
 
-    while (tree->parent != NULL)
-    {
-        depth++;
-        tree = tree->parent;
-    }
-
-    return (depth);
+	while (tree->parent != NULL)
+	{
+	depth++;
+	tree = tree->parent;
+	}
+	return (depth);
 }
